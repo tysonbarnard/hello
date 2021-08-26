961c301a26f58e27e5fab6abe3ff08ef2a783b48
Tyson Barnard <tbarnard3@huskers.unl.edu>
2021-08-26 13:25:41 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tyson Barnard / Joel Toof
+ *         tbarnard3@cse.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple program that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tyson Barnard\n");
+
+  return 0;
+}
